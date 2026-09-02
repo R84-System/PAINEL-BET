497f4957cd8ee7e860f85624c42e64508b069ecb
R84-System <rbsmultivendas@gmail.com>
2026-09-02 09:11:22 -0300

    Update App.py

--- App.py
+++ App.py
@@ -423,6 +423,8 @@ dashboard_html = """
                     <option value="fra.1">🇫🇷 Ligue 1</option>
                     <option value="por.1">🇵🇹 Primeira Liga</option>
                     <option value="ksa.1">🇸🇦 Saudi Pro League</option>
+                    <option value="qat.1">🇶🇦 Qatar Stars League</option>
+                    <option value="uae.1">🇦🇪 UAE Pro League</option>
                     <option value="afc.champions">🌏 AFC Champions League</option>
                     <option value="jpn.1">🇯🇵 J1 League (Japão)</option>
                     <option value="kor.1">🇰🇷 K League 1 (Coreia do Sul)</option>
@@ -457,6 +459,7 @@ dashboard_html = """
             "uefa.champions": "Champions League", "uefa.europa": "Europa League",
             "eng.1": "Premier League", "esp.1": "La Liga", "ita.1": "Serie A (Itália)", "ger.1": "Bundesliga",
             "fra.1": "Ligue 1", "por.1": "Primeira Liga", "ksa.1": "Saudi Pro League",
+            "qat.1": "Qatar Stars League", "uae.1": "UAE Pro League",
             "afc.champions": "AFC Champions League", "jpn.1": "J1 League (Japão)", "kor.1": "K League 1 (Coreia do Sul)",
             "chn.1": "Chinese Super League", "aus.1": "A-League (Austrália)",
             "arg.1": "Liga Profesional", "mex.1": "Liga MX", "col.1": "Campeonato Colombiano",
